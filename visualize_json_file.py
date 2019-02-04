009844ea1ed664f27a81939798c44e4c85553c6e
gabrielpreite <preitegabriel@gmail.com>
2019-02-04 15:41:05 +0100

    coco api completo

--- visualize_json_file.py
+++ visualize_json_file.py
@@ -10,7 +10,7 @@ if __name__=='__main__':
     ap.add_argument('--json_file', required=True, help="Path to the JSON dataset file to visualize")
     args = ap.parse_args()
     
-    cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
+    #cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
     
     with open(args.json_file, 'r') as f:    
         obj = json.loads(f.read())
@@ -24,31 +24,33 @@ if __name__=='__main__':
     for img in images:
         anns = [ann for ann in annotations if ann["image_id"]==img["id"]]
         image_cv2 = cv2.imread(img["file_name"])
-        cv2.imshow('hey', image_cv2)
-        cv2.waitKey()
+        
         #print img
         #print ann
         #print ann['bbox']
                
        
         for ann in anns:
-           s = [int(x) for x in ann['bbox']]
+            s = [int(x) for x in ann['bbox']]
+            print s
            
-           # pts = [pnt for pnt in ann['segmentation']]
-           for pnt in ann['segmentation']:
-               a = np.array(pnt, np.int32)
-               at =  a.reshape((-1,1,2))
-               #print at
-               cv2.polylines(image_cv2,[at],False,(255,0,255),4)
-
+            # pts = [pnt for pnt in ann['segmentation']]
+            for pnt in ann['segmentation']:
+                a = np.array(pnt, np.int32)
+                at = a.reshape((-1,1,2))
+                #print at
+                cv2.polylines(image_cv2,at,False,(255,0,255),4)
           
-           cv2.rectangle(image_cv2, (s[0], s[1]), (s[0]+s[2], s[1]+s[3]), (0,255,255), 8)
-           
-           ProfileClass = classes[ann['category_id']]
-          # ProfileClass = classes[ann['category_id']]
-           print ProfileClass['name']
-           #ProfileClass = unicodedata.normalize('NFKD',Profile).encode('ascii','ignore')
-           cv2.putText(image_cv2, ProfileClass['name'], (s[0]+20, s[1]-20), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 0), 5)
-        
+            cv2.rectangle(image_cv2, (s[0], s[1]), (s[0]+s[2], s[1]+s[3]), (0,255,255), 8)
+            #cv2.imshow('hey', image_cv2)
+            #cv2.waitKey()
+            ProfileClass = classes[ann['category_id']]
+            # ProfileClass = classes[ann['category_id']]
+            print ProfileClass['name']
+            #ProfileClass = unicodedata.normalize('NFKD',Profile).encode('ascii','ignore')
+            cv2.putText(image_cv2, ProfileClass['name'], (s[0], s[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+            #cv2.putText(image_cv2, ProfileClass['name'], (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 0), 5)
+
         cv2.imshow('frame', image_cv2)
-        cv2.waitKey()
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
